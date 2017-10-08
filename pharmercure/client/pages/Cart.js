de999343556d5b1ce58726145342e431c4bb459f
gian50 <gianluca.abate@edu.itspiemonte.it>
2017-10-08 13:22:29 +0200

    first commit

---
+++ pharmercure/client/pages/Cart.js
@@ -0,0 +1,51 @@
+import { Carrello } from './Banco.js'
+import { Drugs } from './Banco.js'
+import { Mongo } from 'meteor/mongo'
+Meteor.subscribe('ordini');
+Ordini = new Mongo.Collection('ordini');
+
+function formatPrice(price) {
+	var decimalPart = price - Math.floor(price);
+	if(decimalPart == 0) {
+		return "" + Math.floor(price) +".00";
+	}
+	else if(decimalPart < 10) {
+		return "" + Math.floor(price) +"." + Math.round(decimalPart*100);
+	}
+
+	return Math.round(price*Math.pow(10,2))/Math.pow(10,2);
+}
+
+
+Template.Cart.helpers({
+	cart: function() {
+		return Carrello.find();
+	},
+	confermaOpen: function() {
+		return Session.get('confermaOrdineOpen');
+	}
+});
+
+Template.Cart.events({
+	'click .close-icon': function() {
+		Carrello.remove(this._id);
+	},
+	'click .inoltra-ordine-btn': function() {
+			console.log('cose');
+		if(Carrello.find().fetch().length != 0){
+			if(Session.get('currentCap') != ''){
+				Ordini.insert({ cap: Session.get('currentCap'), cart: Carrello.find().fetch(), createdAt: new Date(), user: Meteor.userId(), status: "In attesa"});
+				//Session.set('confermaOrdineOpen', 'conferma-ordine-open');
+				alert('Ordine effettuato');
+				Session.set('newOrder', 'play');
+				Session.set('newOrder', '');
+				Carrello.remove({});
+			} else {
+				alert('Inserire CAP');
+			}
+			
+		}else {
+			alert("Carrello vuoto. Selezionare almeno un prodotto per effettuare l'ordine."); 
+		}
+	}
+});
